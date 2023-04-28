708f1de506d2a8bf0b2523a707ec6ea45fe2b77f
bbairdgen <bbairdgen@gmail.com>
2023-04-27 22:34:10 -0600

    adding to do items

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('1. PUT route');
+    
+  const contentDb = await openDB('jate', 1);
+  console.log('1', contentDb);
+  
+  const tx = contentDb.transaction('jate', 'readwrite')
+
+  const store = tx.objectStore('jate')
+  const request = store.put({ id: 1, content: content })
+  const result = await request
+  console.log(result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('1. GET route');
+ const contentDb = await openDB('jate', 1);
+  console.log('1', contentDb);
+ 
+  const tx = contentDb.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const request = store.getAll()
+  const result = await request
+  console.log(('result.value', result));
+
+  return result?.value
+}
 
 initdb();
